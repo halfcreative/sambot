ee8c6d45b1514d10ad724a7a0613bc0ddffcf035
Alan Sato <sam.alan.sato@gmail.com>
2020-06-26 11:18:55 -0500

    remove token from here

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const loginToken = "NzI2MDk5NTE3Mzg0MTYzNDA4.XvYXUg.eWhe-t-ffOFddxVCugnqHq6uvIQ";
 
 fs.readdir("./events/", (err, files) => {
     files.forEach(file => {
