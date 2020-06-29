21b0b512f5b441ff058623feb8ca6c2c582fdba5
Alan Sato <sam.alan.sato@gmail.com>
2020-06-29 16:21:32 -0500

    not yet ready

--- commands/rpg/createChar.js
+++ commands/rpg/createChar.js
@@ -8,22 +8,24 @@ module.exports = async function createChar(msg, splitMessage) {
 
 
 
-    let character = new Character(msg.author);
-    character.setName(splitMessage[1]);
+    // let character = new Character(msg.author);
+    // character.setName(splitMessage[1]);
 
-    let char = await mongoDB.getChar(msg.author);
-    console.log(char);
-    if (char) {
-        msg.channel.send('You already have a character');
-        msg.channel.send(`character name : ${char.name}`);
-    } else {
-        console.log(character);
-        await mongoDB.saveChar(msg.author, character).then(success => {
-            msg.channel.send('Character Successfully Created!');
-        }
-        ).catch(err => {
-            msg.channel.send('Failed to create new character');
-        });
-    }
+    // let char = await mongoDB.getChar(msg.author);
+    // console.log(char);
+    // if (char) {
+    //     msg.channel.send('You already have a character');
+    //     msg.channel.send(`character name : ${char.name}`);
+    // } else {
+    //     console.log(character);
+    //     await mongoDB.saveChar(msg.author, character).then(success => {
+    //         msg.channel.send('Character Successfully Created!');
+    //     }
+    //     ).catch(err => {
+    //         msg.channel.send('Failed to create new character');
+    //     });
+    // }
+
+    msg.channel.send(`Command is still in development. Yell at Sam to finish what he started`);
 
 }
