3ca8e6f07cc3101826c4b69b893747c882203673
Alan Sato <sam.alan.sato@gmail.com>
2020-09-14 19:12:00 -0500

    timer

--- commands/misc/timer.js
+++ commands/misc/timer.js
@@ -1,13 +1,17 @@
 module.exports = function dtimer(msg, splitMessage) {
     const rng = seedrandom();
-    if (splitMessage[1] == 'short') {
-        let time = 1000 * 60 * 10;
+    if (splitMessage[1] == parseInt(splitMessage[1])) {
+        let time = 1000 * 60 * parseInt(splitMessage[1]);
+        msg.channel.send(`${msg.author}, setting timer for ${parseInt(splitMessage[1])} minutes.`);
         let x = setTimeout(function () { msg.channel.send(`${msg.author}, your drop is ready in 5 minutes. Grab now while you can.`); }, time);
-        msg.channel.send(`${msg.author}, your roll is : ${roll}/${splitMessage[1]}`);
+    } else if (splitMessage[1] == 'short') {
+        let time = 1000 * 60 * 10;
+        msg.channel.send(`${msg.author}, setting timer for 10 minutes.`);
+        let x = setTimeout(function () { msg.channel.send(`${msg.author}, your drop is ready in 5 minutes. Grab now while you can.`); }, time);
     } else if (!splitMessage[1]) {
         let time = 1000 * 60 * 20;
+        msg.channel.send(`${msg.author}, setting timer for 20 minutes.`);
         let x = setTimeout(function () { msg.channel.send(`${msg.author}, your drop is ready in 10 minutes. Grab now while you can.`); }, time);
-        msg.channel.send(`${msg.author}, your roll is : ${roll}/100`);
     } else {
         msg.reply(':x: invalid input for .dtimer :x:');
     }
