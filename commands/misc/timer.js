8809622f342f3d0e6b8ed85c207592d4e3833344
Alan Sato <sam.alan.sato@gmail.com>
2020-09-14 19:41:02 -0500

    tori fix

--- commands/misc/timer.js
+++ commands/misc/timer.js
@@ -1,8 +1,15 @@
 module.exports = function dtimer(msg, splitMessage) {
     if (splitMessage[1] == parseInt(splitMessage[1])) {
-        let time = 1000 * 60 * parseInt(splitMessage[1]);
-        msg.channel.send(`${msg.author}, setting timer for ${parseInt(splitMessage[1])} minutes.`);
-        let x = setTimeout(function () { msg.channel.send(`${msg.author}, your drop is ready in 5 minutes. Grab now while you can.`); }, time);
+        if (parseInt(splitMessage[1]) < 120) {
+            let time = 1000 * 60 * parseInt(splitMessage[1]);
+            msg.channel.send(`${msg.author}, setting timer for ${parseInt(splitMessage[1])} minutes.`);
+            let x = setTimeout(function () { msg.channel.send(`${msg.author}, your drop is ready in 5 minutes. Grab now while you can.`); }, time);
+        } else {
+            if (msg.author.username == "torinora") {
+                msg.reply("Why are you like this?");
+            }
+            msg.channel.send(`${msg.author}, requested time is too long. The limit for the timer is 2 hours.`)
+        }
     } else if (splitMessage[1] == 'short') {
         let time = 1000 * 60 * 10;
         msg.channel.send(`${msg.author}, setting timer for 10 minutes.`);
