be939418cea941a02b401d0bda0204fe33327fec
Alan Sato <sam.alan.sato@gmail.com>
2020-09-14 19:50:44 -0500

    adam's update

--- commands/misc/timer.js
+++ commands/misc/timer.js
@@ -11,13 +11,17 @@ module.exports = function dtimer(msg, splitMessage) {
             msg.channel.send(`${msg.author}, requested time is too long. The limit for the timer is 2 hours.`)
         }
     } else if (splitMessage[1] == 'short') {
-        let time = 1000 * 60 * 10;
+        let timex = 1000 * 60 * 10;
+        let timey = 1000 * 60 * 5;
         msg.channel.send(`${msg.author}, setting timer for 10 minutes.`);
-        let x = setTimeout(function () { msg.channel.send(`${msg.author}, your drop is ready in 5 minutes. Grab now while you can.`); }, time);
+        let y = setTimeout(function () { msg.channel.send(`${msg.author}, your drop is ready in 10 minutes. 5 minutes 'till last call.`); }, timey);
+        let x = setTimeout(function () { msg.channel.send(`${msg.author}, your drop is ready in 5 minutes. Last call for a grab.`); }, timex);
     } else if (!splitMessage[1]) {
-        let time = 1000 * 60 * 20;
+        let timex = 1000 * 60 * 20;
+        let timey = 1000 * 60 * 15;
         msg.channel.send(`${msg.author}, setting timer for 20 minutes.`);
-        let x = setTimeout(function () { msg.channel.send(`${msg.author}, your drop is ready in 10 minutes. Grab now while you can.`); }, time);
+        let y = setTimeout(function () { msg.channel.send(`${msg.author}, your drop is ready in 15 minutes. 5 minutes 'till last call.`); }, timey);
+        let x = setTimeout(function () { msg.channel.send(`${msg.author}, your drop is ready in 10 minutes. Last call for a grab.`); }, timex);
     } else {
         msg.reply(':x: invalid input for .dtimer :x:');
     }
