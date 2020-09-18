a66f42f2c5c3b5af23ebe2d919f1405edacea16d
Alan Sato <sam.alan.sato@gmail.com>
2020-09-18 15:32:00 -0500

    update msg

--- commands/karutaAssist/id.js
+++ commands/karutaAssist/id.js
@@ -2,6 +2,8 @@ module.exports = function roll(msg, splitMessage) {
     console.log('msg', msg);
     if (splitMessage[1]) {
         console.log(splitMessage[1]);
+        console.log('msg mentions', msg.mentions);
+    } else {
+        msg.channel.send(`${msg.author}, your ID is : ${msg.author.id}`);
     }
-    msg.channel.send(`${msg.author}, your ID is : ${msg.author.id}`);
 }
