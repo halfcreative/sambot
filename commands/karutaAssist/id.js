84bee0ec54ed7d4d6d6666bdd0c1b77eed98fd2a
Alan Sato <sam.alan.sato@gmail.com>
2020-09-18 15:52:41 -0500

    log

--- commands/karutaAssist/id.js
+++ commands/karutaAssist/id.js
@@ -7,8 +7,8 @@ module.exports = function roll(msg, splitMessage) {
             if (msg.mentions.everyone) {
                 msg.channel.send(`Sam has forbidden me from checking the ID of everyone.`);
             } else {
-
                 for (let user of msg.mentions.users) {
+                    console.log(user);
                     msg.channel.send(`${user.userName} has a user id of ${user.id}`);
                 }
             }
