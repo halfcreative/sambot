90d7384b87c5abe6a91dd68609b80e24d0feb785
Alan Sato <sam.alan.sato@gmail.com>
2020-09-18 20:58:34 -0500

    more dev

--- commands/misc/timer.js
+++ commands/misc/timer.js
@@ -9,18 +9,25 @@ module.exports = function dtimer(msg, splitMessage, isAdmin) {
             switch (splitMessage[1]) {
                 case 'clear':
                     msg.channel.send('clearing out timers');
-                    timers = timers.filter(timer => timer.author != msg.author.id);
+                    clearTimers(msg.author.id);
+                    // timers = timers.filter(timer => timer.author != msg.author.id);
                     break;
                 case 'view':
                     if (isAdmin) {
-
                     }
+                    msg.channel.send(`There are ${timers.length} timers in the timer array`);
                     break;
             }
         }
     } else {
-        let timeout = setTimeout(function () { msg.channel.send(`${msg.author}, your timer is ready `); }, 1000);
-        timers.push({ author: msg.author.id, timer: timeout });
-        msg.channel.send(`${timers.length}`);
+        let LastCallTimeout = setTimeout(function () { msg.channel.send(`${msg.author}, your drop will come up in 15 minutes. Last call to grab a card.`); }, 1000);
+        let Timeout = setTimeout(function () { msg.channel.send(`${msg.author}, your drop will come up in 10 minutes. Avoid grabbing anything unless necessary.`); clearTimers(); }, 1000);
+        timers.push({ author: msg.author.id, timer: Timeout });
+        timers.push({ author: msg.author.id, timer: LastCallTimeout });
+        msg.channel.send(`Setting 2 timers. You will recieve a 'last call' reminder in 15 minutes, and a 'cutoff' reminder in 20.`);
     }
+}
+
+function clearTimers(id) {
+    timers = timers.filter(timer => timer.author != id)
 }
