bed30ed5ae2e1228a818a02489010440f08ea521
Alan Sato <sam.alan.sato@gmail.com>
2020-10-09 11:25:14 -0500

    typo

--- commands/karutaAssist/pray.js
+++ commands/karutaAssist/pray.js
@@ -1,7 +1,7 @@
 const { pray } = require("../../services/mongoService");
 
 module.exports = async function prayer(msg) {
-    let results = await pray(msg.auther);
+    let results = await pray(msg.author);
     console.log(results);
     msg.channel.send('Praise Craig! Praise Brian!');
 }
