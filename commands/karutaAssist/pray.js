c192bdae33525090b9e9329a8efab17c5e22ea95
Alan Sato <sam.alan.sato@gmail.com>
2020-10-09 17:08:30 -0500

    ok

--- commands/karutaAssist/pray.js
+++ commands/karutaAssist/pray.js
@@ -6,7 +6,8 @@ module.exports = async function prayer(msg) {
         msg.channel.send('Praise Craig! Praise Brian!');
         msg.channel.send(`You are now devotion level ${results.userPrayObj.prayers}`);
     } else {
-        msg.channel.send(` ${msg.author}, you are praying too much. You must wait ${results.userPrayObj.lastPrayed / 1000} seconds before praying again`);
+        const now = Date.now();
+        msg.channel.send(` ${msg.author}, you are praying too much. You must wait ${(now - results.userPrayObj.lastPrayed) / 1000} seconds before praying again`);
         msg.channel.send(`Your devotion level is ${results.userPrayObj.prayers}`);
     }
 }
