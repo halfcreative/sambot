08d51224795e27ccd3fe79e6d5546f215e81cbf0
Alan Sato <sam.alan.sato@gmail.com>
2020-10-09 11:12:57 -0500

    more changes

--- services/mongoService.js
+++ services/mongoService.js
@@ -5,15 +5,18 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 module.exports = {
 
     pray: async function (user) {
+        let results = null;
         try {
             await client.connect();
             const collection = client.db("sambot").collection("prayers");
-            const results = await collection.updateOne({ 'user': player.id }, { $inc: { prayers: 1 } });
-            client.close();
-            return results;
+            results = await collection.updateOne({ 'user': player.id }, { $inc: { prayers: 1 } });
         } catch (e) {
             console.log(e);
+        } finally {
+
+            client.close();
         }
+        return results;
     },
 
 }
