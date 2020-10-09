e7d06e46942357144a218ab2447a46bcb4e2cf58
Alan Sato <sam.alan.sato@gmail.com>
2020-10-09 15:52:58 -0500

    pray

--- commands/karutaAssist/pray.js
+++ commands/karutaAssist/pray.js
@@ -4,5 +4,5 @@ module.exports = async function prayer(msg) {
     let results = await pray(msg.author);
     console.log(results);
     msg.channel.send('Praise Craig! Praise Brian!');
-    msg.channel.send(`You have prayed ${results.prayers}`)
+    msg.channel.send(`You are now devotion level ${results.prayers}`);
 }
