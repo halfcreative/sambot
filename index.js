20334a9c7b4fb8a8774b838807254977857c3343
Alan Sato <sam.alan.sato@gmail.com>
2020-10-08 20:36:14 -0500

    actually call the thing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const Discord = require("discord.js");
 const express = require("express");
 const client = new Discord.Client();
-const wakeupService = require("./services/wakeupService"); // my module!
+const wakeUpDyno = require("./services/wakeupService"); // my module!
 
 
 const PORT = 3000; // whatever port you like
@@ -12,7 +12,7 @@ const DYNO_URL = "https://sambot0036.herokuapp.com"; // the url of your dyno
 const app = express(); // instantiate Express app
 
 app.listen(PORT, () => {
-    wakewakeUpDyno(DYNO_URL);
+    wakeUpDyno(DYNO_URL);
 })
 fs.readdir("./events/", (err, files) => {
     files.forEach(file => {
