d63faaf564b566576b306d7a5f734f8d2cfac7f0
Alan Sato <sam.alan.sato@gmail.com>
2020-10-09 16:25:31 -0500

    pray too much

--- commands/karutaAssist/pray.js
+++ commands/karutaAssist/pray.js
@@ -2,7 +2,10 @@ const { pray } = require("../../services/mongoService");
 
 module.exports = async function prayer(msg) {
     let results = await pray(msg.author);
-    console.log(results);
-    msg.channel.send('Praise Craig! Praise Brian!');
-    msg.channel.send(`You are now devotion level ${results.prayers}`);
+    if (results.success) {
+        msg.channel.send('Praise Craig! Praise Brian!');
+        msg.channel.send(`You are now devotion level ${results.userPrayObj.prayers}`);
+    } else {
+        msg.channel.send(`You're praying too much, try again later`);
+    }
 }
--- services/mongoService.js
+++ services/mongoService.js
@@ -6,24 +6,29 @@ module.exports = {
     pray: async function (user) {
         const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
         let prayers = null;
+        const returnObj = {
+            userPrayObj: null,
+            success: false
+        }
         try {
             await client.connect();
             const collection = client.db("sambot").collection("prayers");
             const userIsDevoted = await collection.findOne({ 'user': user.id });
             console.log(userIsDevoted);
             const now = Date.now();
-            // if (userIsDevoted?.lastPrayed < now + 60000) {
-            //     const results = await collection.updateOne({ 'user': user.id }, { $inc: { prayers: 1 }, $set: { lastPrayed: new Date().now() } }, { upsert: true });
-            //     prayers = await collection.findOne({ 'user': user.id });
-            //     console.log(results);
-            //     console.log(prayers);
-            // }
+            if (userIsDevoted?.lastPrayed < now + 60000) {
+                returnObj.userPrayObj = userIsDevoted;
+            } else {
+                const results = await collection.updateOne({ 'user': user.id }, { $inc: { prayers: 1 }, $set: { lastPrayed: new Date().now() } }, { upsert: true });
+                returnObj.userPrayObj = await collection.findOne({ 'user': user.id });
+                returnObj.success = true;
+            }
         } catch (e) {
             console.log(e);
         } finally {
             await client.close();
         }
-        return prayers;
+        return returnObj;
     },
 
 }
