6c8de472afb1f73fb2621d3ba3406d28e4ded3e9
Alan Sato <sam.alan.sato@gmail.com>
2020-10-09 19:48:53 -0500

    try this

--- services/mongoService.js
+++ services/mongoService.js
@@ -8,6 +8,7 @@ module.exports = {
         let prayers = null;
         const returnObj = {
             userPrayObj: null,
+            rankUpgrade: false,
             success: false
         }
         try {
@@ -18,9 +19,13 @@ module.exports = {
             if (userIsDevoted && userIsDevoted.lastPrayed > (now - 300000)) {
                 returnObj.userPrayObj = userIsDevoted;
             } else {
-                const results = await collection.updateOne({ 'user': user.id }, { $inc: { prayers: 1 }, $set: { lastPrayed: now } }, { upsert: true });
+                const results = await collection.updateOne({ 'user': user.id }, { $inc: { prayers: 1 }, $set: { lastPrayed: now, rank: rankings((userIsDevoted.prayers + 1)) } }, { upsert: true });
                 returnObj.userPrayObj = await collection.findOne({ 'user': user.id });
+                if (userIsDevoted.rank != returnObj.userPrayObj.rank) {
+                    returnObj.rankUpgrade = true;
+                }
                 returnObj.success = true;
+
             }
         } catch (e) {
             console.log(e);
@@ -33,5 +38,28 @@ module.exports = {
 }
 
 function rankings(count) {
+    if (count > 20000) {
+        return 'God';
+    } else if (count > 7500) {
+        return 'Pope';
+    } else if (count > 5000) {
+        return 'Cardinal';
+    } else if (count > 2500) {
+        return 'Archbishop';
+    } else if (count > 1250) {
+        return 'Bishop';
+    } else if (count > 750) {
+        return 'High Priest';
+    } else if (count > 500) {
+        return 'Low Priest';
+    } else if (count > 250) {
+        return 'Devotee';
+    } else if (count > 100) {
+        return 'Follower';
+    } else if (count > 50) {
+        return 'Peasant';
+    } else {
+        return 'Heathen';
+    }
 
 }
