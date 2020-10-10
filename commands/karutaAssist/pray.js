e922e4c7eab8c6eb6ede55485c536600b318702b
Alan Sato <sam.alan.sato@gmail.com>
2020-10-09 20:12:16 -0500

    ok

--- commands/karutaAssist/pray.js
+++ commands/karutaAssist/pray.js
@@ -4,8 +4,8 @@ const { pray } = require("../../services/mongoService");
 module.exports = async function prayer(msg) {
     let results = await pray(msg.author);
     if (results.success) {
-        msg.channel.send('Praise Craig! Praise Brian!');
         const prayerMessage = new MessageEmbed();
+        msg.channel.send('Praise Craig! Praise Brian!');
         prayerMessage.setTitle('Prayer Successful');
         let message = `You are now devotion level ${results.userPrayObj.prayers} \n`;
         if (results.rankUpgrade) {
@@ -17,8 +17,12 @@ module.exports = async function prayer(msg) {
         prayerMessage.setDescription(message);
         msg.channel.send(prayerMessage);
     } else {
+        const prayerMessage = new MessageEmbed();
         const now = Date.now();
-        msg.channel.send(` ${msg.author}, you are praying too much. You must wait ${((results.userPrayObj.lastPrayed + 300000) - now) / 1000} seconds before praying again`);
-        msg.channel.send(`Your devotion level is ${results.userPrayObj.prayers} and your rank is ${results.userPrayObj.rank}`);
+        prayerMessage.setTitle('Prayer Failed');
+        let message = `${msg.author}, you are praying too much. You must wait ${((results.userPrayObj.lastPrayed + 300000) - now) / 1000} seconds before praying again \n`;
+        message += `Your devotion level is ${results.userPrayObj.prayers} and your rank is ${results.userPrayObj.rank}`
+        prayerMessage.setDescription(message);
+        msg.channel.send(prayerMessage);
     }
 }
