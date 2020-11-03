06825bb8afcdf4f7ea8c6cf3ba3346279db7f4c6
Alan Sato <sam.alan.sato@gmail.com>
2020-11-02 18:32:31 -0600

    get sorted results for member contribution

--- services/mongoService.js
+++ services/mongoService.js
@@ -51,7 +51,7 @@ module.exports = {
         try {
             await client.connect();
             const collection = client.db("sambot").collection("clanRecord");
-            const tmpUsers = await collection.find({ 'userId': { $exists: true } }).toArray();
+            const tmpUsers = await collection.find({ 'userId': { $exists: true } }).sort({ totalAddedPower: -1 }).toArray();
             users = tmpUsers;
         } catch (error) {
             console.log(error);
