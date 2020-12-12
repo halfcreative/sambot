ba97b0452678573593665e195d8dac05a84658d1
Alan Sato <sam@localhost.localdomain>
2020-12-12 12:58:08 -0600

    message attachment

--- commands/karutaAssist/noBotCommandsInGeneral.js
+++ commands/karutaAssist/noBotCommandsInGeneral.js
@@ -1,4 +1,7 @@
+const { MessageAttachment } = require("discord.js");
+
 module.exports = function noBotCommandsInGeneral(msg) {
+
     if (msg.channel.name != "anime-degens-nsfw") {
         const attachment = new MessageAttachment('https://media1.tenor.com/images/69d1f3a27cb2dc1fcf0f39dd2e4fe641/tenor.gif?itemid=18284220');
         // Send the attachment in the message channel
