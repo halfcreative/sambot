fe818902e875288170f71d08e118ceb029ecd233
Alan Sato <sam@localhost.localdomain>
2020-12-14 10:26:10 -0600

    ok

--- commands/karutaAssist/noBotCommandsInGeneral.js
+++ commands/karutaAssist/noBotCommandsInGeneral.js
@@ -5,6 +5,6 @@ module.exports = function noBotCommandsInGeneral(msg) {
     if (msg.channel.name != "anime-degens-nsfw") {
         const attachment = new MessageAttachment('https://media1.tenor.com/images/69d1f3a27cb2dc1fcf0f39dd2e4fe641/tenor.gif?itemid=18284220');
         // Send the attachment in the message channel
-        message.channel.send(attachment);
+        msg.channel.send(attachment);
     }
 }
