688a301a1d505eb662553d0007bf1759fb46fff4
Alan Sato <halfcreative@gmail.com>
2020-12-17 14:04:33 -0600

    get correct thumb

--- commands/wowAssist/getCharacter.js
+++ commands/wowAssist/getCharacter.js
@@ -7,7 +7,7 @@ module.exports = async function getWowCharacter(msg) {
     const characterJSON = await getCharacter(userChar.realm, userChar.character);
     const messageEmbed = new MessageEmbed();
     messageEmbed.setTitle(`Character Details for ${characterJSON.name}`);
-    messageEmbed.setThumbnail('https://render-us.worldofwarcraft.com/character/stormrage/180/234904756-avatar.jpg?alt=wow/static/images/2d/avatar/37-0.jpg');
+    messageEmbed.setThumbnail(`${characterJSON.thumbnail_url}`);
     let characterMessage = `${characterJSON.name}, the ${characterJSON.active_spec_name} ${characterJSON.class} \n`;
     for (const season of characterJSON.mythic_plus_scores_by_season) {
         messageEmbed.addField(`Mythic+ Season ${season.season.split('-')[2]} Scores :`, '\u200B', false);
