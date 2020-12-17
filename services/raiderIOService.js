a312ca31546521748e04a477c8677404a55385b7
Alan Sato <halfcreative@gmail.com>
2020-12-17 11:56:57 -0600

    fix

--- services/raiderIOService.js
+++ services/raiderIOService.js
@@ -1,6 +1,6 @@
 const fetchUrl = require("fetch").fetchUrl;
 
-export function getCharacter() {
+module.exports = function getCharacter() {
     fetchUrl(`https://raider.io/api/v1/characters/profile?region=us&realm=stormrage&name=pipopapo`).then(response => {
         console.log(response);
     })
