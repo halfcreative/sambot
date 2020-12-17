6649c66a209e40af473381161047dda8b662b750
Alan Sato <halfcreative@gmail.com>
2020-12-17 10:32:58 -0600

    adjust noBotCommands structure

--- events/message.js
+++ events/message.js
@@ -49,23 +49,7 @@ module.exports = async (client, msg) => {
                 break;
             case '.ticker':
                 ticker(msg, splitMessage);
-                breakmodule.exports = function id(msg, splitMessage) {
-                    if (splitMessage[1]) {
-                        console.log(splitMessage[1]);
-                        console.log('msg mentions', msg.mentions);
-                        if (msg.mentions) {
-                            if (msg.mentions.everyone) {
-                                msg.channel.send(`Sam has forbidden me from checking the ID of everyone.`);
-                            } else {
-                                for (let user of msg.mentions.users) {
-                                    msg.channel.send(`${user[1].username} has a user id of ${user[1].id}`);
-                                }
-                            }
-                        }
-                    } else {
-                        msg.channel.send(`${msg.author}, your ID is : ${msg.author.id}`);
-                    }
-                }
+                break;
             case '.id':
                 id(msg, splitMessage);
                 break;
