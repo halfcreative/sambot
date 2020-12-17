b450f578f3504b9bf3176eb8d689ad524a1b01f7
Alan Sato <halfcreative@gmail.com>
2020-12-17 12:18:12 -0600

    ok

--- commands/wowAssist/getCharacter.js
+++ commands/wowAssist/getCharacter.js
@@ -6,6 +6,7 @@ module.exports = async function getWowCharacter(msg) {
     console.log(characterJSON);
     const messageEmbed = new MessageEmbed();
     messageEmbed.setTitle(`Character Details for ${characterJSON.name}`);
+    messageEmbed.setImage('https://render-us.worldofwarcraft.com/character/stormrage/180/234904756-avatar.jpg?alt=wow/static/images/2d/avatar/37-0.jpg');
     messageEmbed.setDescription(`${characterJSON}`);
     msg.channel.send(messageEmbed);
 
