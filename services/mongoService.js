fd411b34d79aad30cde3cbc670f222efeb54d7dc
Alan Sato <halfcreative@gmail.com>
2020-12-17 13:21:36 -0600

    try this

--- services/mongoService.js
+++ services/mongoService.js
@@ -167,7 +167,7 @@ module.exports = {
         let users = [];
         try {
             await client.connect();
-            const collection = client.db("sambot").collection("prayers");
+            const collection = client.db("sambot").collection("wowChar");
             const tmpUsers = await collection.find({ user: user.id }).toArray();
             users = tmpUsers;
         } catch (error) {
