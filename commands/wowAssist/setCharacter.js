5f8d2d13d0cd98ae7f27da834828486fd71ed492
Alan Sato <halfcreative@gmail.com>
2020-12-17 13:32:56 -0600

    ok

--- commands/wowAssist/setCharacter.js
+++ commands/wowAssist/setCharacter.js
@@ -1,9 +1,9 @@
 const { MessageEmbed } = require('discord.js');
-const { setCharacter } = require('../../services/mongoService');
+const { setUserCharacter } = require('../../services/mongoService');
 
 module.exports = async function setWowCharacter(msg, splitMessage) {
     if (splitMessage[1] && splitMessage[2]) {
-        setCharacter(msg.author, splitMessage[1], splitMessage[2]);
+        setUserCharacter(msg.author, splitMessage[1], splitMessage[2]);
         msg.channel.send(`setting server to ${splitMessage[1]}, and character to ${splitMessage[2]}`)
     } else {
         msg.channel.send('Not Enough Parameters Passed : .setChar servername charactername');
