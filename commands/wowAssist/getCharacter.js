92912f128f84546c3a5cfdb04f623a3107c58085
Alan Sato <halfcreative@gmail.com>
2020-12-17 12:16:07 -0600

    wait for response

--- commands/wowAssist/getCharacter.js
+++ commands/wowAssist/getCharacter.js
@@ -1,8 +1,8 @@
 const { MessageEmbed } = require('discord.js');
 const getCharacter = require('../../services/raiderIOService');
 
-module.exports = function getWowCharacter(msg) {
-    const characterJSON = getCharacter();
+module.exports = async function getWowCharacter(msg) {
+    const characterJSON = await getCharacter();
     console.log(characterJSON);
     const messageEmbed = new MessageEmbed();
     messageEmbed.setTitle(`Character Details for ${characterJSON.name}`);
