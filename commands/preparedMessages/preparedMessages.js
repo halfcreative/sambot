d62fbdd2eee4f76eb10825605a12719d08f3b5a0
Alan Sato <halfcreative@gmail.com>
2020-12-23 11:25:24 -0600

    asdf

--- commands/preparedMessages/preparedMessages.js
+++
@@ -1,6 +0,0 @@
-const { MessageAttachment } = require("discord.js");
-
-module.exports = function noBotCommands(msg) {
-    const attachment = new MessageAttachment('https://media1.tenor.com/images/69d1f3a27cb2dc1fcf0f39dd2e4fe641/tenor.gif?itemid=18284220');
-    msg.channel.send(attachment);
-}
