a4f6fbcf133ed4de7e06b81268bf9c9b2b87e8d5
Alan Sato <halfcreative@gmail.com>
2021-07-21 14:06:43 -0500

    try

--- commands/karutaAssist/stalk.js
+++ commands/karutaAssist/stalk.js
@@ -1,3 +1,5 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports = async function stalk(client, msg, splitMessage) {
     if (splitMessage[1]) {
         console.log(splitMessage[1]);
@@ -8,7 +10,14 @@ module.exports = async function stalk(client, msg, splitMessage) {
         let x = await client.users.fetch(splitMessage[1]);
         console.log(x);
         console.log("send");
-        msg.channel.send(`${splitMessage[1]} is associated with user ${x}`);
+
+
+        const stalkReport = new MessageEmbed();
+        stalkReport.setTitle(`Stalking Report for user with ID (${splitMessage[1]})`);
+        stalkReport.setThumbnail(x.displayAvatarUrl);
+        stalkReport.setDescription(`${splitMessage[1]} is associated with user ${x.username}#${x.discriminator}`)
+        msg.channel.send(stalkReport);
+
         // let x = await msg.channel.users.fetch(splitMessage[1]);
         // console.log(x);
         // msg.channel.send(`${splitMessage[1]} belongs to user ${x}`);
