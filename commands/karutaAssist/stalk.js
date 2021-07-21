3c1adcc9dc5e865eb493b03718c353e6bdea7889
Alan Sato <halfcreative@gmail.com>
2021-07-21 13:36:21 -0500

    try

--- commands/karutaAssist/stalk.js
+++ commands/karutaAssist/stalk.js
@@ -1,9 +1,10 @@
-module.exports = function stalk(client, msg, splitMessage) {
+module.exports = async function stalk(client, msg, splitMessage) {
     if (splitMessage[1]) {
         console.log(splitMessage[1]);
         console.log(msg);
         console.log(client);
-        console.log((await client.users.fetch(splitMessage[1])));
+        console.log((client.users.fetch(splitMessage[1])));
+        let x = await client.users.fetch(splitMessage[1]);
         // let x = await msg.channel.users.fetch(splitMessage[1]);
         // console.log(x);
         // msg.channel.send(`${splitMessage[1]} belongs to user ${x}`);
