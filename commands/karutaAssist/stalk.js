ee935be588a23cb679d221e6d717b3b371e897e8
Alan Sato <halfcreative@gmail.com>
2021-07-21 12:52:20 -0500

    try

--- commands/karutaAssist/stalk.js
+++ commands/karutaAssist/stalk.js
@@ -2,9 +2,11 @@ module.exports = function stalk(msg, splitMessage) {
     if (splitMessage[1]) {
         console.log(splitMessage[1]);
         console.log('msg mentions', msg.mentions);
-        let x = await msg.channel.users.fetch(splitMessage[1]);
-        console.log(x);
-        msg.channel.send(`${splitMessage[1]} belongs to user ${x}`);
+        console.log(msg.channel.users)
+        console.log(msg.channel.users.fetch(splitMessage[1]));
+        // let x = await msg.channel.users.fetch(splitMessage[1]);
+        // console.log(x);
+        // msg.channel.send(`${splitMessage[1]} belongs to user ${x}`);
     } else {
         msg.channel.send(`${msg.author}, pass an ID like: .stalk ${msg.author.id}`);
     }
