c4b97b67c3940e8dd180c02c57cda62c17ee5919
Alan Sato <halfcreative@gmail.com>
2021-08-02 20:00:03 -0500

    try

--- events/message.js
+++ events/message.js
@@ -98,6 +98,9 @@ module.exports = async (client, msg) => {
                     description: 'fuckin fight me bitch!',
                 }
                 const command = await client.guilds.cache.get('469682998619406353')?.commands.create(data);
+                console.log(msg.guild);
+                console.log(client.guilds.cache);
+                console.log(client.guilds.cache.get('469682998619406353'));
                 console.log(command);
                 break;
             default:
