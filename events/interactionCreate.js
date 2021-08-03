2365ce230d788c7541c9823c8049a98df57ccca1
Alan Sato <halfcreative@gmail.com>
2021-08-02 19:12:49 -0500

    try me

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,7 +1,7 @@
 module.exports = async (client, interaction) => {
     if (!interaction.isCommand()) return;
 
-    if (interaction.commandName === 'ping') {
-        await interaction.reply('Pong!');
+    if (interaction.commandName === 'tryMe') {
+        await interaction.reply('later');
     }
 }
--- events/messageCreate.js
+++ events/messageCreate.js
@@ -3,8 +3,8 @@ module.exports = async client => {
 
     if (message.content.toLowerCase() === '!deploy' && message.author.id === client.application?.owner.id) {
         const data = {
-            name: 'ping',
-            description: 'Replies with Pong!',
+            name: 'tryMe',
+            description: 'fuckin fight me bitch!',
         };
 
         const command = await client.guilds.cache.get('469682998619406353')?.commands.create(data);
