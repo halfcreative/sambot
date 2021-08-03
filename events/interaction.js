6c6f0d8d4ea6b5df678bb6797257d93d16791886
Alan Sato <halfcreative@gmail.com>
2021-08-02 19:25:42 -0500

    more fix

--- events/interaction.js
+++ events/interaction.js
@@ -1,7 +1,7 @@
 module.exports = async (client, interaction) => {
     if (!interaction.isCommand()) return;
 
-    if (interaction.commandName === 'tryMe') {
+    if (interaction.commandName === 'tryme') {
         await interaction.reply('later');
     }
 }
