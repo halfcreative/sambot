d6a1660e1dddfaeab00f877f1723138a9623118e
Alan Sato <halfcreative@gmail.com>
2022-02-15 12:22:36 -0600

    export

--- services/mongoService.js
+++ services/mongoService.js
@@ -1,187 +1,184 @@
 import MongoClient from 'mongodb';
 const uri = process.env.MONGO_URL;
 
-export default {
+export async function currentAttackCycle() {
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    let attackCycleNumber = 0;
+    try {
+        await client.connect();
+        const collection = client.db("sambot").collection("clanRecord");
+        const attackRecord = await collection.findOne({ 'clanRecordType': 'attack' });
+        attackCycleNumber = attackRecord.attackCycle;
+    } catch (error) {
+        console.log(error);
+    } finally {
+        client.close();
+    }
 
-    currentAttackCycle: async function () {
-        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-        let attackCycleNumber = 0;
-        try {
-            await client.connect();
-            const collection = client.db("sambot").collection("clanRecord");
-            const attackRecord = await collection.findOne({ 'clanRecordType': 'attack' });
-            attackCycleNumber = attackRecord.attackCycle;
-        } catch (error) {
-            console.log(error);
-        } finally {
-            client.close();
-        }
-
-        return attackCycleNumber;
-    },
+    return attackCycleNumber;
+}
 
 
-    clanAttack: async function () {
-        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-        let result;
-        try {
-            await client.connect();
-            const collection = client.db("sambot").collection("clanRecord");
-            const attackRecord = await collection.findOne({ 'clanRecordType': 'attack' });
-            const now = Date.now();
-            // if (attackRecord.lastAttacked > (now - 1800000)) {
-            //     console.log("attacking so soon?");
-            //     // Working on a new attack cycle
-            //     result = await collection.updateOne({ 'clanRecordType': 'attack' }, { $set: { lastAttacked: now } }, { upsert: true });
-            // } else {
-            console.log("normal attack")
-            result = await collection.updateOne({ 'clanRecordType': 'attack' }, { $inc: { attackCycle: 1 }, $set: { lastAttacked: now } }, { upsert: true });
-            // }
-        } catch {
+export async function clanAttack() {
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    let result;
+    try {
+        await client.connect();
+        const collection = client.db("sambot").collection("clanRecord");
+        const attackRecord = await collection.findOne({ 'clanRecordType': 'attack' });
+        const now = Date.now();
+        // if (attackRecord.lastAttacked > (now - 1800000)) {
+        //     console.log("attacking so soon?");
+        //     // Working on a new attack cycle
+        //     result = await collection.updateOne({ 'clanRecordType': 'attack' }, { $set: { lastAttacked: now } }, { upsert: true });
+        // } else {
+        console.log("normal attack")
+        result = await collection.updateOne({ 'clanRecordType': 'attack' }, { $inc: { attackCycle: 1 }, $set: { lastAttacked: now } }, { upsert: true });
+        // }
+    } catch {
 
-        } finally {
-            client.close();
-        }
-        return result;
-    },
+    } finally {
+        client.close();
+    }
+    return result;
+}
 
-    getMemberContributions: async function () {
-        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-        let users = [];
-        try {
-            await client.connect();
-            const collection = client.db("sambot").collection("clanRecord");
-            users = await collection.find({ 'userId': { $exists: true } }).sort({ totalAddedPower: -1 }).toArray();
-        } catch (error) {
-            console.log(error);
-        } finally {
-            client.close();
-        }
+export async function getMemberContributions() {
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    let users = [];
+    try {
+        await client.connect();
+        const collection = client.db("sambot").collection("clanRecord");
+        users = await collection.find({ 'userId': { $exists: true } }).sort({ totalAddedPower: -1 }).toArray();
+    } catch (error) {
+        console.log(error);
+    } finally {
+        client.close();
+    }
 
-        return users;
-    },
+    return users;
+}
 
-    recordWork: async function (userId, power) {
-        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-        let result;
-        try {
-            await client.connect();
-            const collection = client.db("sambot").collection("clanRecord");
-            const attackRecord = await collection.findOne({ 'clanRecordType': 'attack' });
-            const user = await collection.findOne({ 'userId': userId });
-            const now = Date.now();
-            if (user) {
-                if (attackRecord.attackCycle != user.attackCycle) {
-                    console.log("new attack cycle");
-                    // Working on a new attack cycle
-                    if (user.lastWorked > (now - 39600000)) {
-                        //last worked variable is less than 11 hours ago, must not have committed last work command.
-                        console.log("working so soon?");
-                        result = await collection.updateOne({ 'userId': userId }, { $inc: { runningTotalAllCycles: (power - user.lastAddedPower) }, $set: { totalAddedPower: power, lastWorked: now, lastAddedPower: power, attackCycle: attackRecord.attackCycle, workCount: 1 } }, { upsert: true });
-                    } else {
-                        console.log("normal work ");
-                        result = await collection.updateOne({ 'userId': userId }, { $inc: { runningTotalAllCycles: power, workCountAllCycles: 1 }, $set: { totalAddedPower: power, lastWorked: now, lastAddedPower: power, attackCycle: attackRecord.attackCycle, workCount: 1 } }, { upsert: true });
-                    }
+export async function recordWork(userId, power) {
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    let result;
+    try {
+        await client.connect();
+        const collection = client.db("sambot").collection("clanRecord");
+        const attackRecord = await collection.findOne({ 'clanRecordType': 'attack' });
+        const user = await collection.findOne({ 'userId': userId });
+        const now = Date.now();
+        if (user) {
+            if (attackRecord.attackCycle != user.attackCycle) {
+                console.log("new attack cycle");
+                // Working on a new attack cycle
+                if (user.lastWorked > (now - 39600000)) {
+                    //last worked variable is less than 11 hours ago, must not have committed last work command.
+                    console.log("working so soon?");
+                    result = await collection.updateOne({ 'userId': userId }, { $inc: { runningTotalAllCycles: (power - user.lastAddedPower) }, $set: { totalAddedPower: power, lastWorked: now, lastAddedPower: power, attackCycle: attackRecord.attackCycle, workCount: 1 } }, { upsert: true });
                 } else {
-                    console.log("current attack cycle");
-                    if (user.lastWorked > (now - 39600000)) {
-                        //last worked variable is less than 11 hours ago, must not have committed last work command.
-                        console.log("working so soon?");
-                        result = await collection.updateOne({ 'userId': userId }, { $inc: { runningTotalAllCycles: (power - user.lastAddedPower), totalAddedPower: (power - user.lastAddedPower) }, $set: { lastWorked: now, lastAddedPower: power } }, { upsert: true });
-                    } else {
-                        console.log("normal work");
-                        result = await collection.updateOne({ 'userId': userId }, { $inc: { runningTotalAllCycles: power, totalAddedPower: power, workCount: 1, workCountAllCycles: 1 }, $set: { lastWorked: now, lastAddedPower: power } }, { upsert: true });
-                    }
+                    console.log("normal work ");
+                    result = await collection.updateOne({ 'userId': userId }, { $inc: { runningTotalAllCycles: power, workCountAllCycles: 1 }, $set: { totalAddedPower: power, lastWorked: now, lastAddedPower: power, attackCycle: attackRecord.attackCycle, workCount: 1 } }, { upsert: true });
                 }
             } else {
-                result = await collection.updateOne({ 'userId': userId }, { $inc: { runningTotalAllCycles: power, totalAddedPower: power, workCount: 1, workCountAllCycles: 1 }, $set: { lastWorked: now, lastAddedPower: power, workCount: 1, attackCycle: attackRecord.attackCycle } }, { upsert: true });
-            }
-        } catch {
-
-        } finally {
-            client.close();
-        }
-        return result;
-    },
-    pray: async function (user) {
-        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-        let prayers = null;
-        const returnObj = {
-            userPrayObj: null,
-            rankUpgrade: false,
-            success: false
-        }
-        try {
-            await client.connect();
-            const collection = client.db("sambot").collection("prayers");
-            const userIsDevoted = await collection.findOne({ 'user': user.id });
-            const now = Date.now();
-            if (userIsDevoted && userIsDevoted.lastPrayed > (now - 300000)) {
-                returnObj.userPrayObj = userIsDevoted;
-            } else {
-                if (userIsDevoted) {
-                    const results = await collection.updateOne({ 'user': user.id }, { $inc: { prayers: 1 }, $set: { lastPrayed: now, rank: rankings((userIsDevoted.prayers + 1)) } }, { upsert: true });
+                console.log("current attack cycle");
+                if (user.lastWorked > (now - 39600000)) {
+                    //last worked variable is less than 11 hours ago, must not have committed last work command.
+                    console.log("working so soon?");
+                    result = await collection.updateOne({ 'userId': userId }, { $inc: { runningTotalAllCycles: (power - user.lastAddedPower), totalAddedPower: (power - user.lastAddedPower) }, $set: { lastWorked: now, lastAddedPower: power } }, { upsert: true });
                 } else {
-                    const results = await collection.updateOne({ 'user': user.id }, { $inc: { prayers: 1 }, $set: { lastPrayed: now, rank: rankings(1) } }, { upsert: true });
+                    console.log("normal work");
+                    result = await collection.updateOne({ 'userId': userId }, { $inc: { runningTotalAllCycles: power, totalAddedPower: power, workCount: 1, workCountAllCycles: 1 }, $set: { lastWorked: now, lastAddedPower: power } }, { upsert: true });
                 }
-                returnObj.userPrayObj = await collection.findOne({ 'user': user.id });
-                if (userIsDevoted.rank != returnObj.userPrayObj.rank) {
-                    returnObj.rankUpgrade = true;
-                }
-                returnObj.success = true;
             }
-        } catch (e) {
-            console.log(e);
-        } finally {
-            await client.close();
+        } else {
+            result = await collection.updateOne({ 'userId': userId }, { $inc: { runningTotalAllCycles: power, totalAddedPower: power, workCount: 1, workCountAllCycles: 1 }, $set: { lastWorked: now, lastAddedPower: power, workCount: 1, attackCycle: attackRecord.attackCycle } }, { upsert: true });
         }
-        return returnObj;
-    },
+    } catch {
 
-    viewChurch: async function () {
-        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-        let users = [];
-        try {
-            await client.connect();
-            const collection = client.db("sambot").collection("prayers");
-            const tmpUsers = await collection.find({}).sort({ prayers: -1 }).toArray();
-            users = tmpUsers;
-        } catch (error) {
-            console.log(error);
-        } finally {
-            client.close();
+    } finally {
+        client.close();
+    }
+    return result;
+}
+
+export async function pray(user) {
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    let prayers = null;
+    const returnObj = {
+        userPrayObj: null,
+        rankUpgrade: false,
+        success: false
+    }
+    try {
+        await client.connect();
+        const collection = client.db("sambot").collection("prayers");
+        const userIsDevoted = await collection.findOne({ 'user': user.id });
+        const now = Date.now();
+        if (userIsDevoted && userIsDevoted.lastPrayed > (now - 300000)) {
+            returnObj.userPrayObj = userIsDevoted;
+        } else {
+            if (userIsDevoted) {
+                const results = await collection.updateOne({ 'user': user.id }, { $inc: { prayers: 1 }, $set: { lastPrayed: now, rank: rankings((userIsDevoted.prayers + 1)) } }, { upsert: true });
+            } else {
+                const results = await collection.updateOne({ 'user': user.id }, { $inc: { prayers: 1 }, $set: { lastPrayed: now, rank: rankings(1) } }, { upsert: true });
+            }
+            returnObj.userPrayObj = await collection.findOne({ 'user': user.id });
+            if (userIsDevoted.rank != returnObj.userPrayObj.rank) {
+                returnObj.rankUpgrade = true;
+            }
+            returnObj.success = true;
         }
+    } catch (e) {
+        console.log(e);
+    } finally {
+        await client.close();
+    }
+    return returnObj;
+}
 
-        return users;
-    },
+export async function viewChurch() {
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    let users = [];
+    try {
+        await client.connect();
+        const collection = client.db("sambot").collection("prayers");
+        const tmpUsers = await collection.find({}).sort({ prayers: -1 }).toArray();
+        users = tmpUsers;
+    } catch (error) {
+        console.log(error);
+    } finally {
+        client.close();
+    }
 
-    setUserCharacter: async function (user, realmName, characterName) {
-        console.log(user);
-        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    return users;
+}
+
+export async function setUserCharacter(user, realmName, characterName) {
+    console.log(user);
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    await client.connect();
+    const collection = client.db("sambot").collection("wowChar");
+    const results = await collection.updateOne({ 'user': user.id }, { $set: { realm: realmName, character: characterName } }, { upsert: true });
+    client.close();
+}
+
+export async function getUserCharacter(userId) {
+    console.log('user', userId);
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    let userChar;
+    try {
         await client.connect();
         const collection = client.db("sambot").collection("wowChar");
-        const results = await collection.updateOne({ 'user': user.id }, { $set: { realm: realmName, character: characterName } }, { upsert: true });
+        const result = await collection.findOne({ 'user': userId });
+        userChar = result;
+    } catch (error) {
+        console.log(error);
+    } finally {
         client.close();
-    },
-
-    getUserCharacter: async function (userId) {
-        console.log('user', userId);
-        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-        let userChar;
-        try {
-            await client.connect();
-            const collection = client.db("sambot").collection("wowChar");
-            const result = await collection.findOne({ 'user': userId });
-            userChar = result;
-        } catch (error) {
-            console.log(error);
-        } finally {
-            client.close();
-        }
-        console.log('userChar', userChar);
-        return userChar;
-    },
-
+    }
+    console.log('userChar', userChar);
+    return userChar;
 }
 
 function rankings(count) {
