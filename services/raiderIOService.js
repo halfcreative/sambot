7801314ff4d049f854a0c41a5b71c1a8a2f3ca23
Alan Sato <halfcreative@gmail.com>
2022-02-15 13:35:27 -0600

    no default

--- services/raiderIOService.js
+++ services/raiderIOService.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 
-export default function getIO(realm, name) {
+export function getIO(realm, name) {
     return fetch(`https://raider.io/api/v1/characters/profile?region=us&realm=${realm}&name=${name}&fields=mythic_plus_scores_by_season%3Acurrent%2Cmythic_plus_ranks%2Cmythic_plus_best_runs%2Cmythic_plus_recent_runs%2Cgear`).then(response => response.json());
 }
 export function getCharacter(realm, name) {
