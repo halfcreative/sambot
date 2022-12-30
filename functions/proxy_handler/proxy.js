812fa917244ef51807787fcf8f3b7f29ddb543b2
Alan Sato <halfcreative@gmail.com>
2022-12-29 20:41:22 -0600

    Get proxy working on ping

--- functions/proxy_handler/proxy.js
+++ functions/proxy_handler/proxy.js
@@ -1,18 +1,18 @@
 import { SNSClient, PublishCommand } from "@aws-sdk/client-sns";
+import nacl from "tweetnacl";
 
 const REGION = process.env.REGION;
 const snsClient = new SNSClient({ region: REGION });
-const nacl = require('tweetnacl');
 
-exports.handler = async (event) => {
+export async function handler(event, context, callback) {
     const strBody = event.body; // should be string, for successful sign
 
     if (!event.headers['test']) {
+        console.info("verifying signature");
         // Checking signature (requirement 1.)
         const PUBLIC_KEY = process.env.PUBLIC_KEY;
         const signature = event.headers['x-signature-ed25519'] || event.headers['X-Signature-Ed25519'];
         const timestamp = event.headers['x-signature-timestamp'] || event.headers['X-Signature-Timestamp'];
-
 
         const isVerified = nacl.sign.detached.verify(
             Buffer.from(timestamp + strBody),
@@ -21,6 +21,7 @@ exports.handler = async (event) => {
         );
 
         if (!isVerified) {
+            console.info("Unverified request signature");
             return {
                 statusCode: 401,
                 body: JSON.stringify('invalid request signature'),
