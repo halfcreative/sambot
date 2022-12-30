7ae6bea322abd7793949764ef86cbde57147d6b5
Alan Sato <halfcreative@gmail.com>
2022-12-29 20:04:35 -0600

    No need for services in 2.0

--- services/raiderIOService.js
+++
@@ -1,8 +0,0 @@
-import fetch from 'node-fetch';
-
-export function getIO(realm, name) {
-    return fetch(`https://raider.io/api/v1/characters/profile?region=us&realm=${realm}&name=${name}&fields=mythic_plus_scores_by_season%3Acurrent%2Cmythic_plus_ranks%2Cmythic_plus_best_runs%2Cmythic_plus_recent_runs%2Cgear`).then(response => response.json());
-}
-export function getCharacter(realm, name) {
-    return fetch(`https://raider.io/api/v1/characters/profile?region=us&realm=${realm}&name=${name}&fields=gear%2Ccovenant%2Cguild`).then(response => response.json());
-}
