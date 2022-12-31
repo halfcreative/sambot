b80bc9070e84ce11e1499507e13395269b70668c
Alan Sato <halfcreative@gmail.com>
2022-12-30 20:27:57 -0600

    remove comment

--- functions/pray/pray.js
+++ functions/pray/pray.js
@@ -57,31 +57,3 @@ export async function handler(event) {
             console.error(error);
         });
 }
-
-// import { MessageEmbed } from "discord.js";
-// import { pray } from '../../services/mongoService.js';
-// async function prayer(msg) {
-//     let results = await pray(msg.author);
-//     if (results.success) {
-//         const prayerMessage = new MessageEmbed();
-//         prayerMessage.setTitle(':pray: Prayer Successful :pray:');
-//         let message = `You are now devotion level ${results.userPrayObj.prayers} \n`;
-//         if (results.rankUpgrade) {
-//             message += `*notices your rank* OwO wHat's THIS???? You got a new rank UwU ! \n`;
-//             message += `Your rank is now ${results.userPrayObj.rank}\n`;
-//             message += `uwu owo uwu`;
-//         } else {
-//             message += `Your rank is ${results.userPrayObj.rank}`;
-//         }
-//         prayerMessage.setDescription(message);
-//         msg.channel.send(prayerMessage);
-//     } else {
-//         const prayerMessage = new MessageEmbed();
-//         const now = Date.now();
-//         prayerMessage.setTitle('Prayer Failed');
-//         let message = `${msg.author}, you are praying too much. You must wait ${((results.userPrayObj.lastPrayed + 300000) - now) / 1000} seconds before praying again \n`;
-//         message += `Your devotion level is ${results.userPrayObj.prayers} and your rank is ${results.userPrayObj.rank}`
-//         prayerMessage.setDescription(message);
-//         msg.channel.send(prayerMessage);
-//     }
-// }
